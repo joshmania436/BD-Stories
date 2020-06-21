2987460b68f27b4a11d9053cce5cdf0f1c995c3b
josh-web <joshuamrahi@gmail.com>
2020-06-21 14:20:38 +0530

    Updated Screens

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return(
+      <AppContainer />
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  WriteStory: WriteStoryScreen,
+  ReadStory: ReadStoryScreen
+}
+  )
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import {Header} from 'react-native-elements';
+
+export default class WriteStoryScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            author: '',
+            storyText: '',
+        }
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Header 
+                    backgroundColor = {'black'}
+                    centerComponent = {{
+                        text : 'Bed Time Stories',
+                        style : { color: 'white', fontSize: 20}
+                    }}
+                />
+                <TextInput 
+                    placeholder="Story Title"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            title: text
+                        })
+                    }}
+                    value={this.state.title}
+                    style={styles.title}/>
+                <TextInput
+                    placeholder="Author"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            author: text
+                        })
+                    }}
+                    value={this.state.author}
+                    style={styles.author} />
+                <TextInput 
+                    placeholder="Write your story"
+                    onChangeText= {(text)=>{
+                        this.setState({
+                            storyText: text
+                        })
+                    }}
+                    value={this.state.storyText}
+                    style={styles.storyText}
+                    multiline={true}/>
+                
+                <TouchableOpacity
+                    style={styles.submitButton}>
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  title:{
+      height: 40,
+      borderWidth: 2,
+      marginTop: 40,
+      margin: 10
+  },
+  author: {
+      height: 40,
+      borderWidth: 2,
+      margin: 10
+  },
+  storyText: {
+      height: 250,
+      borderWidth: 2,
+      margin: 10
+  },
+  submitButton:{
+      justifyContent: 'center',
+      alignSelf: 'center',
+      backgroundColor: 'black',
+      width: 180,
+      height: 90,
+      borderRadius:150
+  },
+  buttonText: {
+      textAlign: 'center',
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize:20
+  }
+});

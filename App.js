4b4c044ffc54cddec0a7aa851fc6a0d87f63fad4
Joshua <55012411+josh-web@users.noreply.github.com>
2020-06-30 23:01:23 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -16,16 +16,30 @@ export default class App extends React.Component {
 const TabNavigator = createBottomTabNavigator({
   WriteStory: WriteStoryScreen,
   ReadStory: ReadStoryScreen
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName =navigation.state.routeName;
+      if(routeName === "WriteStory"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40,height:40}}
+          />)
+      }
+      else if(routeName === "ReadStory"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
 }
   )
 
 const AppContainer = createAppContainer(TabNavigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

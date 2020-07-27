9f6767a5bb68d46238759e25afc2967db3d10ea6
Joshua <55012411+josh-web@users.noreply.github.com>
2020-07-27 16:29:47 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import LoginScreen from './screens/LoginScreen';
 import WriteStoryScreen from './screens/WriteStoryScreen'
 import ReadStoryScreen from './screens/ReadStoryScreen'
 
@@ -18,28 +19,42 @@ const TabNavigator = createBottomTabNavigator({
   ReadStory: ReadStoryScreen
 },
 {
-  defaultNavigationOptions:({navigation})=>({
-    tabBarIcon:()=>{
-      const routeName =navigation.state.routeName;
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
       if(routeName === "WriteStory"){
-        return(
-          <Image
-          source={require("./assets/read.png")}
-          style={{width:40,height:40}}
-          />)
-      }
-      else if(routeName === "ReadStory"){
         return(
           <Image
           source={require("./assets/write.png")}
           style={{width:40, height:40}}
+        />
+        )
+
+      }
+      else if(routeName === "ReadStory"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
         />)
-        
+
       }
     }
   })
-}
-  )
+})
 
-const AppContainer = createAppContainer(TabNavigator);
+const SwitchNavigator = createSwitchNavigator({
+  LoginScreen : LoginScreen,
+  TabNavigator : TabNavigator
+})
 
+const AppContainer = createAppContainer(SwitchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
